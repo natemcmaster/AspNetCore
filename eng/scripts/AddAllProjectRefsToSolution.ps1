59fbe809bdcf820141d81971a41ac0e528f6cb5a
Nate McMaster <nate.mcmaster@microsoft.com>
2019-01-25 17:23:15 -0800

    Add script to make it easier to add all projects to a .sln

---
+++ eng/scripts/AddAllProjectRefsToSolution.ps1
@@ -0,0 +1,44 @@
+<#
+.SYNOPSIS
+This adds the complete closure of project references to a .sln file
+#>
+param(
+    [string]$WorkingDir,
+    [string]$SolutionFile
+)
+
+$ErrorActionPreference = 'Stop'
+$repoRoot = Resolve-Path "$PSScriptRoot/../../"
+$listFile = New-TemporaryFile
+
+if (-not $WorkingDir) {
+    $WorkingDir = Get-Location
+}
+
+Push-Location $WorkingDir
+try {
+    if (-not $SolutionFile) {
+
+        $slnCount = Get-ChildItem *.sln | Measure
+
+        if ($slnCount.count -eq 0) {
+            Write-Error "Could not find a solution in this directory. Specify one with -SolutionFile <PATH>"
+            exit 1
+        }
+        if ($slnCount.count -gt 1) {
+            Write-Error "Multiple solutions found in this directory. Specify which one to modify with -SolutionFile <PATH>"
+            exit 1
+        }
+        $SolutionFile = Get-ChildItem *.sln | select -first 1
+    }
+
+    & "$repoRoot\build.ps1" -projects "$(Get-Location)\**\*.*proj" /t:ShowProjectClosure "/p:ProjectsReferencedOutFile=$listFile"
+
+    foreach ($proj in (Get-Content $listFile)) {
+        & dotnet sln $SolutionFile add $proj
+    }
+}
+finally {
+    Pop-Location
+    rm $listFile -ea ignore
+}
