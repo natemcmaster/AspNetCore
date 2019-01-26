fce83051f4a0ad5deec18b92839f67cf732120f8
Nate McMaster <nate.mcmaster@microsoft.com>
2019-01-26 09:46:33 -0800

    Use KnownFrameworkReference to override targeting pack resolution

--- startvs.cmd
+++ startvs.cmd
@@ -3,7 +3,7 @@
 :: This command launches a Visual Studio solution with environment variables required to use a local version of the .NET Core SDK.
 
 :: This tells .NET Core to use the same dotnet.exe that build scripts use
-SET DOTNET_ROOT=%~dp0\x64
+SET DOTNET_ROOT=%~dp0\.dotnet\x64
 
 :: This tells .NET Core not to go looking for .NET Core in other places
 SET DOTNET_MULTILEVEL_LOOKUP=0
